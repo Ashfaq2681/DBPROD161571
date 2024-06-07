00166b7dce01f6a4d4e65ad3959f7e9fe30c48c2
malikrizwan177 <malik.rizwan177@yahoo.com>
2024-06-07 17:38:00 +0500

    cors origin added

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://harry-photo-frontend.vercel.app/',
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(express.static('uploads'))
 
 app.use((req, res, next) => {
