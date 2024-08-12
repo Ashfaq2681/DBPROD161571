6deb5991e2944f7b51d6be9718909370cf7ce9ba
blucomtechnologies <ashfaq2681@gmail.com>
2024-08-12 23:47:28 +0500

    files to setup initial repository

--- assets/Success-CglGOG2S.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ChLwve22.js";const t=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Success"});export{t as default};
