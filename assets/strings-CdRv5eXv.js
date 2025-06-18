9ccc711bc3012554922a0a18c050f536e0625b92
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-06-18 15:21:21 +0500

    new

---
+++ assets/strings-CdRv5eXv.js
@@ -0,0 +1,1 @@
+const a="https://api.curatedgallery.org",s=`${a}/api`,o=`${a}/uploads`;export{o as a,s as b};
--- assets/strings-DDEdFAqL.js
+++
@@ -1,1 +0,0 @@
-const a="http://api.curatedgallery.org",s=`${a}/api`,o=`${a}/uploads`;export{o as a,s as b};
