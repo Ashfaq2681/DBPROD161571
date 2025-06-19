e298d6637965362140c315aebdf2e1fbf69e328c
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-06-19 20:48:47 +0500

    new

--- assets/Success-CWpYj--T.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BeAfN6Kj.js";const t=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Success"});export{t as default};
