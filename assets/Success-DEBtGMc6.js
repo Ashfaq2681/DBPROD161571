e0724698158e0623f7eccb071745e1b2ebe19500
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-06-21 16:25:37 +0500

    new

--- assets/Success-DEBtGMc6.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Cz8qRBh1.js";const t=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Success"});export{t as default};
