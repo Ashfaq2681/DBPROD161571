7db9c525360446ee644277b92cdf512d363770e6
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-06-21 16:04:08 +0500

    new

---
+++ assets/Success-BUXJrEbW.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-ByDMkcKB.js";const t=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Success"});export{t as default};
--- assets/strings-CdRv5eXv.js
+++
@@ -1,1 +0,0 @@
-const a="https://api.curatedgallery.org",s=`${a}/api`,o=`${a}/uploads`;export{o as a,s as b};
