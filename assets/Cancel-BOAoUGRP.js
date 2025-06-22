f8bd624fa7bcee9b7f5aaf19e6500683a574655c
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-06-22 16:30:01 +0500

    new

---
+++ assets/Cancel-BOAoUGRP.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CS5XGz_V.js";const l=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Failed"});export{l as default};
--- assets/Success-pmt_IAoR.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Bu2VjX5e.js";const t=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Success"});export{t as default};
