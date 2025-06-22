35da16a34bec6cfa6e9a2d8602933ee142357052
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-06-22 16:28:03 +0500

    Changes to be committed: 	modified:   frontend/src/Components/Customers.jsx 	modified:   frontend/src/Components/Designers.jsx

--- frontend/src/Components/Customers.jsx
+++ frontend/src/Components/Customers.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
 import UserTile from './UserTile';
+import { baseUrl } from '../constants/strings';
 
 export default function Customers() {
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
-    setCustomers([
-      {
-        email: "abc@xyz.com"
-      },
-      {
-        email: "xyz@abc.com"
-      }
-    ]);
-  });
+    fetch(`${baseUrl}/user/getCustomers`)
+      .then((res) => res.json())
+      .then((data) => setCustomers(data));
+  }, []);
 
   return (
     <div className='w-full mt-6'>
--- frontend/src/Components/Designers.jsx
+++ frontend/src/Components/Designers.jsx
@@ -1,25 +1,23 @@
 import { useEffect } from 'react';
 import { useState } from 'react'
 import UserTile from './UserTile';
+import { baseUrl } from '../constants/strings';
 
 export default function Designers() {
   const [designers, setDesigners] = useState([]);
 
   useEffect(() => {
-    setDesigners([
-      {
-        email: "design@xyz.com"
-      },
-      {
-        email: "mario@abc.com"
-      }
-    ]);
-  });
+    fetch(`${baseUrl}/user/getDesigners`)
+      .then((res) => res.json())
+      .then((data) => setDesigners(data));
+  }, []);
 
   return (
     <div className='w-full mt-6'>
       <div>
-        {designers.map((customer, index) => {
+        {designers == []
+        ? <div>No designers found</div>
+        : designers.map((customer, index) => {
           return <UserTile
             key={index}
             email={customer.email}/>
