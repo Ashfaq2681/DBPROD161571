a23ae16477aa7d4be6dde9b163a56d79a48397ce
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-06-29 14:13:38 +0500

    new

---
+++ assets/Cancel-CIDIFMLm.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-hol7BXgm.js";const l=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Failed"});export{l as default};
---
+++ assets/Success-B_vFTBR5.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-hol7BXgm.js";const t=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Success"});export{t as default};
